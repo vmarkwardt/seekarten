49b7485ed4df1ad5654124a098a8a30fa197706c
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-19 21:20:09 +0200

    Add AreaLink component

---
+++ src/AreaLink.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  text-decoration: none;
+`;
+
+export default function AreaLink() {
+  return <StyledLink />;
+}
