3e87add215a295a4d1730ab1061241c2bd1c506a
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-20 15:47:12 +0200

    Add index.mdx for StartPage of docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,6 @@
 export const imports = {
+  'index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
   'src/AreaLink.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-area-link" */ 'src/AreaLink.mdx'
