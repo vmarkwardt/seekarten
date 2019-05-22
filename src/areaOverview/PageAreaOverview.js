ae8c0e61ba3d9ecf92756072d5691f56d047064f
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-22 13:18:05 +0200

    Add Basic styling for PageOverview

--- src/areaOverview/PageAreaOverview.js
+++ src/areaOverview/PageAreaOverview.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import AreaLink from '../commons/AreaLink'
+import styled from 'styled-components'
+
+const StyledPageAreaOverview = styled.main`
+  background: #b6e1dd;
+  margin: 5px;
+  padding: 10px;
+`
 
 export default function PageAreaOverview() {
   return (
-    <main>
+    <StyledPageAreaOverview>
       <header className="App-header">
         <h1>Seekarten</h1>
       </header>
       <AreaLink name="Ich" btnColor="#e76f51" path="/ich" />
       <AreaLink name="Wir" btnColor="#f4a261" path="/wir" />
       <AreaLink name="Welt" btnColor="#2a9d8f" path="/welt" />
-    </main>
+    </StyledPageAreaOverview>
   )
 }
