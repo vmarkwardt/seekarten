90e122e60571ebcbe596154b7cf623c491c6747a
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-25 21:53:12 +0200

    Styling CertificateCard adding docz

--- src/certificateOverview/CertificateCard.js
+++ src/certificateOverview/CertificateCard.js
@@ -3,18 +3,33 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 const StyledCertificateCard = styled.section`
-  border: 1px solid darkgray;
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  margin: 5px;
+  box-shadow: 1px 2px 2px 1px rgba(0, 0, 0, 0.1);
+`
+const Header = styled.div`
+  background: #b6e1dd;
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.8em;
+  padding: 5px;
+`
+const Content = styled.section`
+  padding: 5px;
 `
 
 export default function CertificateCard({ title, subject, date, comment }) {
   return (
     <StyledCertificateCard>
-      <>
+      <Header>
         <em>{subject}</em>
         <span>{date}</span>
-      </>
-      <h4>{title}</h4>
-      <p>{comment}</p>
+      </Header>
+      <Content>
+        <h4>{title}</h4>
+        <small>{comment}</small>
+      </Content>
     </StyledCertificateCard>
   )
 }
