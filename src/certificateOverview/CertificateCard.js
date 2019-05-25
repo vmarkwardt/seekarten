392096faf0c634b8d62204f9f995ccdcc709257d
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-25 20:58:28 +0200

    Create children of CertificateCard

--- src/certificateOverview/CertificateCard.js
+++ src/certificateOverview/CertificateCard.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StyledCertificateCard = styled.ul``
+const StyledCertificateCard = styled.li``
 
 export default function CertificateCard({ title, subject, date, comment }) {
   return (
-    <StyledCertificateCard
-      title={title}
-      subject={subject}
-      date={date}
-      comment={comment}
-    />
+    <StyledCertificateCard>
+      <>
+        <em>{subject}</em>
+        <span>{date}</span>
+      </>
+      <h3>{title}</h3>
+      <p>{comment}</p>
+    </StyledCertificateCard>
   )
 }
