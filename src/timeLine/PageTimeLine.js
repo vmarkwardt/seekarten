a081fed2cd17f4eec7032d3c32f7a11822597c93
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-28 18:33:56 +0200

    Add basic timeLine sort and add to stage

--- src/timeLine/PageTimeLine.js
+++ src/timeLine/PageTimeLine.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { H2 } from '../commons/headings'
 
 const StyledPageTimeLine = styled.main``
 
 export default function PageTimeLine({ eventList }) {
+  console.log('PageTimeLine.js EventList: ', eventList)
+
+  /*const firstList = eventList.map(event => ({
+    date: event.date,
+    title: event.title,
+    id
+  }))
+  */
+  const firstList = eventList.slice()
+  firstList.sort((a, b) => (a.date < b.date ? 1 : -1))
+
+  const [filteredEventList, setFilteredEventList] = useState(firstList)
   return (
     <StyledPageTimeLine>
       <header>
         <H2 title="Zeitleiste" />
       </header>
       <ul>
-        {eventList.map(event => (
-          <li>
+        {filteredEventList.map(event => (
+          <li key={event.title}>
             {event.date} {event.title}
           </li>
         ))}
