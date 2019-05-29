db7e7a25b55c2616cbbd431b71b2fe9ee7055732
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-29 21:39:56 +0200

    Change anchor to button in EventFilterButton add styling

--- src/timeLine/EventFilterButton.js
+++ src/timeLine/EventFilterButton.js
@@ -2,14 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const StyledEventFilterButton = styled.a`
+const StyledEventFilterButton = styled.button`
   background: ${props => (props.isactive ? 'whitesmoke' : '#b6e1dd ')};
   color: #2a9d8f;
   border: 2px solid #2a9d8f;
   border-radius: 3px;
   text-decoration: none;
   margin: 5px;
-  padding: 2px;
+  padding: 5px;
+  font-size: 1em;
 
   &.active {
     background: whitesmoke;
