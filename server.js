6017d49868c8eb790bc4fd7b3277a27f22eca711
Vanessa Markwardt <vmarkwardt@web.de>
2019-06-06 17:05:08 +0200

    Add server and services for Backend MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const setupServer = require('./setup-server')
 const app = setupServer()
-//const mongoose = require('mongoose')
 const Charts = require('./models/Charts')
+const Certificates = require('./models/Certificate')
 
-app.get('/test', (req, res) => {
+app.get('/chartList', (req, res) => {
   Charts.find()
     .then(data => res.json(data))
     .catch(err => err.json({ errors: [err] }))
+})
+
+app.patch('/chartList/:id', (req, res) => {
+  const { id } = req.params
+  const newCharts = req.body
+  Charts.findByIdAndUpdate({ _id: id }, newCharts, { new: true }) // to get the updated Obj back
+    .then(charts => res.json(charts))
+    .catch(err => console.log('Something went wrong while updating charts!'))
+})
+
+app.post('/chartList', (req, res) => {
+  Charts.create(req.body)
+    .then(charts => res.status(201).json(charts))
+    .catch(err => res.status(500).json({ erorrs: [err] }))
+})
+
+app.get('/certificates', (req, res) => {
+  Certificates.find()
+    .then(data => res.json(data))
+    .catch(err => err.json({ errors: [err] }))
+})
+
+app.patch('/certificates/:id', (req, res) => {
+  const { id } = req.params
+  const newCertificates = req.body
+  Certificates.findByIdAndUpdate({ _id: id }, newCertificates, { new: true }) // to get the updated Obj back
+    .then(certificates => res.json(certificates))
+    .catch(err =>
+      console.log('Something went wrong while updating certificates!')
+    )
+})
+
+app.post('/certificates', (req, res) => {
+  Certificates.create(req.body)
+    .then(certificates => res.status(201).json(certificates))
+    .catch(err => res.status(500).json({ erorrs: [err] }))
 })
---
+++ src/services.js
@@ -0,0 +1,69 @@
+export async function getCharts() {
+  try {
+    const res = await fetch('/chartList')
+    return await res.json()
+  } catch (e) {
+    console.log('getCharts() at services.js: ', e)
+    return e
+  }
+}
+
+export async function postCharts(chartList) {
+  console.log('postCharts in services.js ', JSON.stringify(chartList))
+  const res = await fetch('/chartList', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(chartList),
+  })
+  return await res.json()
+}
+
+export async function patchCharts(chartList) {
+  const res = await fetch(`/chartList/${chartList._id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(chartList),
+  })
+  return res.json
+}
+
+export async function getCertificates() {
+  try {
+    const res = await fetch('/certificates')
+    return await res.json()
+  } catch (e) {
+    console.log('getCertificates() at services.js: ', e)
+    return e
+  }
+}
+
+export async function postCertificates(certificates) {
+  console.log('postCharts in services.js ', JSON.stringify(certificates))
+  const res = await fetch('/certificates', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(certificates),
+  })
+  return await res.json()
+}
+
+export async function patchCertificates(certificates) {
+  const res = await fetch(`/certificates/${certificates._id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(certificates),
+  })
+  return res.json
+}
+
+export function setLocal(name, data) {
+  localStorage.setItem(name, JSON.stringify(data))
+}
+
+export function getLocal(name) {
+  try {
+    return JSON.parse(localStorage.getItem(name))
+  } catch (error) {
+    console.log(error)
+  }
+}
