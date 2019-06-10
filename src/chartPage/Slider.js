0526979a5054162cc7d74ec26e6bc0e7d0ecfd34
Vanessa Markwardt <vmarkwardt@web.de>
2019-06-10 12:10:26 +0200

    Add css to slider = input range  in order to make it visible in chrome device view

--- src/chartPage/Slider.js
+++ src/chartPage/Slider.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { chartWorldColor } from '../commons/constants'
 
 const StyledSlider = styled.input`
+  opacity: 0.7;
   border-radius: 5px;
   height: 15px;
   margin: 5px;
   padding: 5px;
-  opacity: 0.7;
   outline: none;
   transition: opacity 0.15s ease-in-out;
   width: 50%;
+
+  -webkit-appearance: none;
+  background: transparent;
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    border: 2px solid ${chartWorldColor};
+    background: #ddd;
+    height: 18px;
+    width: 18px;
+    margin-top: -8px;
+    border-radius: 99px;
+  }
+  &::-webkit-slider-runnable-track {
+    height: 5px;
+    background: #ddd;
+    border-radius: 15px;
+  }
 `
 
 export default function Slider({ progress, onProgressChange }) {
