8034360aafd6a04718ccfa4c3b72f835709e5c21
Vanessa Markwardt <vmarkwardt@web.de>
2019-06-12 14:19:37 +0200

    Change error handling in server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.patch('/chartList/:id', (req, res) => {
   const newCharts = req.body
   Charts.findByIdAndUpdate({ _id: id }, newCharts, { new: true }) // to get the updated Obj back
     .then(charts => res.json(charts))
-    .catch(err => console.log('Something went wrong while updating charts!'))
+    .catch(err => res.status(500).json({ erorrs: [err] }))
 })
 
 app.post('/chartList', (req, res) => {
@@ -51,9 +51,7 @@ app.patch('/certificates/:id', (req, res) => {
   console.log('app.patch(/certificates/:id', id, newCertificates)
   Certificates.updateOne({ userId: id }, { certificateList: newCertificates }) // to get the updated Obj back
     .then(certificates => res.json(certificates))
-    .catch(err =>
-      console.log('Something went wrong while updating certificates!', err)
-    )
+    .catch(err => res.status(500).json({ erorrs: [err] }))
 })
 
 app.post('/certificates', (req, res) => {
