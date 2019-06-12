b7cf3ef0ba4ce6767403f41637c5deb0068759c9
Vanessa Markwardt <vmarkwardt@web.de>
2019-06-12 14:15:38 +0200

    Style ButtonSubmit to move in grid and padding

--- src/certificateForm/FormCertificate.js
+++ src/certificateForm/FormCertificate.js
@@ -7,12 +7,17 @@ import InputDate from '../commons/InputDate'
 import { getFormatedDate, getTrimmedEntry } from '../utils'
 import PropTypes from 'prop-types'
 
+const StyledButtonSubmit = styled(ButtonSubmit)`
+  margin: 0;
+  grid-column-end: -1;
+`
+
 const StyledForm = styled.form`
   display: grid;
   grid-gap: 10px;
   grid-template-columns: 1fr 3fr;
+  padding: 10px;
 `
-
 export default function FormCertificate({
   onSubmit,
   editCertificate,
@@ -44,8 +49,6 @@ export default function FormCertificate({
     if (isCertificateValid(newEntry)) {
       setCertificate(getNewCertificate())
       onSubmit(getTrimmedCertificate(newEntry), history)
-    } else {
-      window.alert('Bitte einen Titel eingeben.')
     }
   }
 
@@ -60,7 +63,6 @@ export default function FormCertificate({
     )
     return certCopy
   }
-
   return (
     <StyledForm onSubmit={handleOnSubmit}>
       <label htmlFor="title">Titel:</label>
@@ -68,20 +70,23 @@ export default function FormCertificate({
         value={certificate.title}
         name="title"
         onChange={event => handleInputOnChange('title', event.target.value)}
-        required={true}
+        placeholder="Titel"
+        required="required"
       />
       <label htmlFor={'subject'}>Fach / Thema:</label>
       <InputText
         value={certificate.subject}
         name="subject"
         onChange={event => handleInputOnChange('subject', event.target.value)}
-        required={true}
+        placeholder="Natur"
+        required="required"
       />
       <label htmlFor="date">Datum:</label>
       <InputDate
-        value={certificate.date}
+        value={getFormatedDate(certificate.date)}
         name="date"
         onChange={event => handleInputOnChange('date', event.target.value)}
+        required="required"
       />
       <label htmlFor="comment">Bemerkung:</label>
       <textarea
@@ -91,11 +96,13 @@ export default function FormCertificate({
         cols="33"
         onChange={event => handleInputOnChange('comment', event.target.value)}
       />
-      <ButtonSubmit text={'Speichern'} />
+      <StyledButtonSubmit text={'Speichern'} />
     </StyledForm>
   )
 }
 
+{
+}
 FormCertificate.propType = {
   onSubmit: PropTypes.func.isRequired,
   editCertificate: PropTypes.object.isRequired,
