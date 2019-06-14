d99112babd908e57bd4d3ccf521ba094a08d0e68
Vanessa Markwardt <vmarkwardt@web.de>
2019-06-14 17:49:20 +0200

    Add testing for certificate form

--- cypress/integration/seekarten.spec.js
+++ cypress/integration/seekarten.spec.js
@@ -1,33 +1,78 @@
+const todaysDate = Cypress.moment().format('YYYY-MM-DD')
+const typedText = 'test text'
+
 describe('visit website', function() {
   it('visit seekarten heroku website', function() {
     cy.visit('https://seekarten.herokuapp.com')
     cy.get('h2').should('have.text', 'Seekarte: ICH')
+  })
 
+  it('open page to add certifcate', () => {
     cy.contains('Zertifikat eintragen').click()
     cy.url().should('include', '/certificate')
-
     // test form fields
-    cy.get('[name="title"]')
-      .type('die ist ein Test-Titel')
-      .should('have.value', 'die ist ein Test-Titel')
-    cy.get('[name="subject"]')
-      .type('Test-Thema')
-      .should('have.value', 'Test-Thema')
+    //cy.focused()
+  })
 
-    const todaysDate = Cypress.moment().format('YYYY-MM-DD')
+  it('fill from and save certificate', () => {
+    cy.get('form')
+      .find('[name="title"]')
+      .type(typedText)
+      .should('have.value', typedText)
+    cy.get('[name="subject"]')
+      .type(typedText)
+      .should('have.value', typedText)
+
     cy.get('[name="date"]').should('have.value', todaysDate)
 
     cy.get('[name="comment"]')
-      .type('Dies ist eine Bemerkung!')
-      .should('have.value', 'Dies ist eine Bemerkung!')
+      .type(typedText)
+      .should('have.value', typedText)
 
+    // https://on.cypress.io/submit
+    cy.get('form').submit()
+    cy.contains('Zertifikate')
+  })
+
+  it('check first certificate in list', function() {
+    cy.get('em')
+      .first()
+      .should('have.text', typedText)
+    cy.get('span')
+      .first()
+      .should('have.text', todaysDate)
+    cy.get('h4')
+      .first()
+      .should('have.text', typedText)
+    cy.get('small')
+      .first()
+      .should('have.text', typedText)
+  })
+
+  it('visit seekarten heroku website', function() {
     // Page timeLine
     cy.contains('Zeitleiste').click()
     cy.url().should('include', '/timeLine')
 
-    cy.contains('all').click()
+    //Filter all
+    cy.contains('all').click() //.should('have.class', 'active')
+    //Filter skill
     cy.contains('skill').click()
+    cy.get('h2').should('have.text', 'Zeitleiste ')
+
+    //cy.get('ul section li ')
+    // .should('have.attr')
+    // .should('have.type', 'skill')
+    //cy.get('ul section').should('have.text', 'Arbeitshaltung')
+    //cy.get('ul section li').should('have.text', 'Arbeitshaltung')
+
+    //Filter certificate
     cy.contains('certificate').click()
+
+    // visualisation page
+    cy.contains('Sunburst').click()
+    cy.url().should('include', '/vis')
+    cy.get('.basic-sunburst-example-wrapper')
 
     cy.contains('Seekarten').click()
     cy.url().should('include', '/charts/ich')
